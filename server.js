4f51efff70aff352953df6a2451489f3701cb4c6
seunghyukshin <auseunghyuk@gmail.com>
2021-03-05 18:27:34 +0900

    #7 add a game Object in socket.roomNumber

--- server.js
+++ server.js
@@ -12,31 +12,37 @@ const io = require("socket.io")(http, {
 const port = 8080;
 
 const pc = require("./PedigreeCalculator.js");
+const utils = require("./utils.js");
 // io = [socket, socket]
 let roomNumber = 0;
 const gameRoom = [];
 
 const INIT_HOLD_DICES = [false, false, false, false, false];
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-let turnCount = 0;
-
 io.on("connection", (socket) => {
   console.log("user connection");
-  const game = {
-    score: {},
-    dices: [],
-    holdPedigree: "",
-    rollCount: 0,
-  };
+  // const game = {
+  //   score: {},
+  //   dices: [],
+  //   holdPedigree: "",
+  //   rollCount: 0,
+  // };
 
   socket.on("matching", (data) => {
     if (gameRoom.length) {
       socket.roomNumber = gameRoom.shift();
       socket.join(socket.roomNumber);
 
+      io.to(socket.roomNumber).game = {
+        score: {
+          0: {},
+          1: {},
+        },
+        dices: [],
+        holdPedigree: "",
+        rollCount: 0,
+        turnCount: 0,
+      };
       io.to(socket.roomNumber).emit("matched", "");
 
       // 나를 제외한 방에 있는 사람들에게 turn 부여
@@ -60,30 +66,34 @@ io.on("connection", (socket) => {
     let rollDices = [];
     for (let i = 0; i < 5; i++) {
       if (holddDices[i]) {
-        rollDices.push(game.dices[i]);
+        rollDices.push(io.to(socket.roomNumber).game.dices[i]);
       } else {
-        rollDices.push(getRandomInt(1, 6));
+        rollDices.push(utils.getRandomInt(1, 6));
       }
     }
-    game.dices = rollDices;
+    io.to(socket.roomNumber).game.dices = rollDices;
     io.to(socket.roomNumber).emit("rollDices", rollDices);
 
     // roll count
-    let rollCount = game.rollCount + 1;
-    game.rollCount = rollCount;
+    let rollCount = io.to(socket.roomNumber).game.rollCount + 1;
+    io.to(socket.roomNumber).game.rollCount = rollCount;
     io.to(socket.roomNumber).emit("countRolls", rollCount);
   });
 
   socket.on("submit", () => {
-    const holdPedigreeTitle = game.holdPedigree;
-    const pediScore = pc.calculate(holdPedigreeTitle, game.dices);
+    const holdPedigreeTitle = io.to(socket.roomNumber).game.holdPedigree;
+    const pediScore = pc.calculate(
+      holdPedigreeTitle,
+      io.to(socket.roomNumber).game.dices
+    );
     if (pediScore.holdPedigree) {
       // 이미 존재하는 족보에 대한 처리
       console.log("족보에 이미 기재되어있음");
     }
 
-    game.score[holdPedigreeTitle] = pediScore;
-    console.log(game);
+    const turn = io.to(socket.roomNumber).game.turnCount % 2;
+    io.to(socket.roomNumber).game.score[turn][holdPedigreeTitle] = pediScore;
+    console.log(io.to(socket.roomNumber).game);
 
     // 초기화 작업
     // turn 부여보다 먼저해야지만 화면이 자연스럽다. 수정할 것
@@ -91,28 +101,36 @@ io.on("connection", (socket) => {
 
     io.to(socket.roomNumber).emit("hold", INIT_HOLD_DICES);
 
-    game.rollCount = 0;
-    io.to(socket.roomNumber).emit("countRolls", game.rollCount);
-    turnCount++;
-    console.log("turnCount", turnCount);
+    io.to(socket.roomNumber).game.rollCount = 0;
+    io.to(socket.roomNumber).emit(
+      "countRolls",
+      io.to(socket.roomNumber).game.rollCount
+    );
 
-    io.to(socket.id).emit("updateScore", game.score, true); // 내 점수
+    io.to(socket.id).emit(
+      "updateScore",
+      io.to(socket.roomNumber).game.score[turn],
+      true
+    ); // 내 점수
     socket.broadcast
       .to(socket.roomNumber)
-      .emit("updateScore", game.score, false); // 상대 점수
+      .emit("updateScore", io.to(socket.roomNumber).game.score[turn], false); // 상대 점수
 
-    game.dices = [];
+    io.to(socket.roomNumber).game.dices = [];
     io.to(socket.roomNumber).emit("rollDices", []);
 
     io.to(socket.id).emit("submit", false); // 자신은 turn false.
     socket.broadcast.to(socket.roomNumber).emit("submit", true);
+
+    io.to(socket.roomNumber).game.turnCount++;
+    console.log(io.to(socket.roomNumber).game);
   });
 
   socket.on("hold", (data) => {
     io.to(socket.roomNumber).emit("hold", data);
   });
   socket.on("holdPedigree", (data) => {
-    game.holdPedigree = data;
+    io.to(socket.roomNumber).game.holdPedigree = data;
     io.to(socket.roomNumber).emit("holdPedigree", data);
   });
 });
